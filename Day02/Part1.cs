1c54bbaefc856bc9ff5586deeade2d717e60c4a2
Simon Hellner <85552673+simonhellner@users.noreply.github.com>
2024-12-02 13:08:56 +0100

    Day 02

---
+++ Day02/Part1.cs
@@ -0,0 +1,41 @@
+﻿namespace Day02
+{
+    public static class Part1
+    {
+        public static void Part1Main()
+        {
+            var input = File.ReadAllLines("input.txt");
+
+            int safeLevelsCount = 0;
+            foreach (var level in input)
+            {
+                var levelNumbers = level.Split(' ')
+                    .Select(int.Parse)
+                    .ToList();
+
+                var abc = levelNumbers
+                    .Select((number, index) => 
+                    {
+                        if (index == 0)
+                           return new { Curr = -1, Prev = -1 };
+                        return new { Curr = number, Prev = levelNumbers[index - 1] };
+                    })
+                    .Skip(1);
+
+                var all3StepsOrLess = abc.All(x => Math.Abs(x.Prev - x.Curr) <= 3 && x.Prev != x.Curr);
+                var allDecreasing = abc.All(x => x.Prev > x.Curr);
+                var allIncreasing = abc.All(x => x.Prev < x.Curr);
+
+
+                if (all3StepsOrLess)
+                {
+                    if (allDecreasing || allIncreasing)
+                    {
+                        safeLevelsCount++;
+                    }
+                }
+            }
+            Console.WriteLine($"Part 1 result: {safeLevelsCount}");
+        }
+    }
+}
---
+++ Day02/Part2.cs
@@ -0,0 +1,71 @@
+﻿namespace Day02
+{
+    public static class Part2
+    {
+        public static void Part2Main()
+        {
+            var input = File.ReadAllLines("input.txt");
+
+            int safeLevelsCount = 0;
+            foreach (var level in input)
+            {
+                var levelNumbers = level.Split(' ')
+                    .Select(int.Parse)
+                    .ToList();
+
+                var abc = levelNumbers
+                    .Select((number, index) =>
+                    {
+                        if (index == 0)
+                            return new { Curr = -1, Prev = -1 };
+                        return new { Curr = number, Prev = levelNumbers[index - 1] };
+                    })
+                    .Skip(1);
+
+                var all3StepsOrLess = abc.All(x => Math.Abs(x.Prev - x.Curr) <= 3 && x.Prev != x.Curr);
+                var allDecreasing = abc.All(x => x.Prev > x.Curr);
+                var allIncreasing = abc.All(x => x.Prev < x.Curr);
+
+
+                if (all3StepsOrLess)
+                {
+                    if (allDecreasing || allIncreasing)
+                    {
+                        safeLevelsCount++;
+                        continue;
+                    }
+                }
+
+
+                // Lite brute force HEHEHE, ful-lösning jag veeet
+                for (int i = 0; i < levelNumbers.Count; i++)
+                {
+                    var tempLevelNumbers = levelNumbers.ToList();
+                    tempLevelNumbers.RemoveAt(i);
+                    var abc2 = tempLevelNumbers
+                           .Select((number, index) =>
+                           {
+                               if (index == 0)
+                                   return new { Curr = -1, Prev = -1 };
+                               return new { Curr = number, Prev = tempLevelNumbers[index - 1] };
+                           }).Skip(1);
+
+                    var all3StepsOrLess2 = abc2.All(x => Math.Abs(x.Prev - x.Curr) <= 3 && x.Prev != x.Curr);
+                    var allDecreasing2 = abc2.All(x => x.Prev > x.Curr);
+                    var allIncreasing2 = abc2.All(x => x.Prev < x.Curr);
+
+                    if (all3StepsOrLess2)
+                    {
+                        if (allDecreasing2 || allIncreasing2)
+                        {
+                            safeLevelsCount++;
+                            break;
+                        }
+                    }
+                }
+            }
+            
+            Console.WriteLine($"Part 2 result: {safeLevelsCount}");
+        }
+    }
+}
