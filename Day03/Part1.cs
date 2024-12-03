562f87b9f8f6039ca11df86765f6772901de17aa
Simon Hellner <85552673+simonhellner@users.noreply.github.com>
2024-12-03 08:20:07 +0100

    Day 03

---
+++ Day03/Part1.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Day03;
+
+public static class Part1
+{
+    public static void Part1Main()
+    {
+        var input = File.ReadAllText("input.txt");
+        var regex = new Regex(@"mul\([0-9]{1,3},[0-9]{1,3}\)");
+        var matches = regex.Matches(input);
+
+        int sum = 0;
+        foreach(Match match in matches)
+        {
+            var value = match.Value;
+            var numb1Str = value.Split(',')[0].Split('(')[1];
+            var numb2Str = value.Split(',')[1].Split(')')[0];
+
+            var numb1 = int.Parse(numb1Str);
+            var numb2 = int.Parse(numb2Str);
+            sum += numb1 * numb2;
+        }
+
+        Console.WriteLine($"Part 1 result: {sum}");
+    }
+}
---
+++ Day03/Part2.cs
@@ -0,0 +1,32 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Day03;
+
+public static class Part2
+{
+    public static void Part2Main()
+    {
+        var input = File.ReadAllText("input.txt");
+
+        var regex = new Regex(@"mul\([0-9]{1,3},[0-9]{1,3}\)");
+
+        int sum = 0;
+        foreach (var doInstruction in input.Split("do()"))
+        {
+            var instrcutions = doInstruction.Split("don't()")[0];
+            var matches = regex.Matches(instrcutions);
+            foreach (Match match in matches)
+            {
+                var value = match.Value;
+                var numb1Str = value.Split(',')[0].Split('(')[1];
+                var numb2Str = value.Split(',')[1].Split(')')[0];
+
+                var numb1 = int.Parse(numb1Str);
+                var numb2 = int.Parse(numb2Str);
+                sum += numb1 * numb2;
+            }
+        }
+        
+        Console.WriteLine($"Part 2 result: {sum}");
+    }
+}
